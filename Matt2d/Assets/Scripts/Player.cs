a0d047000fecccbc479ec78fe54ddc960420f739
Matt Sauro <mjsauro@gmail.com>
2020-09-19 19:06:52 -0500

    added logic for a double jump

--- Matt2d/Assets/Scripts/Player.cs
+++ Matt2d/Assets/Scripts/Player.cs
@@ -8,9 +8,10 @@ public class Player : MonoBehaviour
     [SerializeField] private float gravity = 1.3f;
     [SerializeField] private float jumpHeight = 25.0f;
     private CharacterController _controller;
-    
+
     //cached variables
     private float _yVelocity;
+    private bool _canDoubleJump = false;
 
     void Start()
     {
@@ -23,19 +24,36 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * playerSpeed;
         
+        HandleJump();
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+
+        //reset double jump when back to zero
+        if (_controller.isGrounded)
+        {
+            _canDoubleJump = false;
+        }
+    }
+
+    private void HandleJump()
+    {
         if (_controller.isGrounded)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = jumpHeight;
+                _canDoubleJump = true;
             }
+        }
+        else if (_canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            _yVelocity = jumpHeight;
+            _canDoubleJump = false;
         }
         else
         {
             _yVelocity -= gravity;
         }
-
-        velocity.y = _yVelocity;
-        _controller.Move(velocity * Time.deltaTime);
     }
 }
